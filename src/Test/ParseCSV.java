5ebb5841345a6d3c64359963763443ded31c72a3
droov <dhruv.bindal@outlook.com>
2013-10-22 18:09:53 +0800

    Refactored code

--- src/Test/ParseCSV.java
+++ src/Test/ParseCSV.java
@@ -4,98 +4,73 @@ import java.util.*;
 import java.io.*;
 import java.text.*;
 
-
 public class ParseCSV {
 
-    /**
-     * @param args the command line arguments
-     */
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
 
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args)throws Exception {
+		ArrayList<String> headerList = new ArrayList();
+		String filename = "C:/Users/dhruv_000/Downloads/E0.csv";
+		Scanner sc = new Scanner(new File(filename));
+		sc.useDelimiter(",");
+		String line = "", header = "";
+		int numberOfRows = 0;
 
-        
-        ArrayList<String> headerList = new ArrayList();
-        String filename = "C:/Users/dhruv_000/Downloads/E0.csv";
-        Scanner sc=new Scanner(new File(filename));
-        sc.useDelimiter(",");
-        String line="",header=""; int numberOfRows=0;
+		numberOfRows = numberOfRows(filename);
+		System.out.println("number of rows " + numberOfRows);
 
-        numberOfRows = numberOfRows(filename);
-        System.out.println("number of rows "+numberOfRows);
+		header = sc.nextLine();
 
-        header = sc.nextLine();
+		StringTokenizer head = new StringTokenizer(header);
 
-        StringTokenizer head = new StringTokenizer(header);
+		while (head.hasMoreTokens())
+			headerList.add(head.nextToken(","));
 
-        while(head.hasMoreTokens())
-        headerList.add(head.nextToken(","));
+		System.out.println("header size or number of columns: "
+				+ headerList.size());
 
-        System.out.println("header size or number of columns: "+headerList.size());
+		int i = 0;
 
-        int i=0;
-   //     while(i<headerList.size()){
-   //         System.out.println(headerList.get(i));
-   //        i++;
-    //    }
+		String[][] data = new String[numberOfRows][headerList.size()];
+		String token = "";
+		StringTokenizer str;
 
+		System.out.println("number of coulmns " + data[0].length);
 
-        String [][]data = new String[numberOfRows][headerList.size()];
-        String token="";
-        StringTokenizer str;// = new StringTokenizer("");
+		for (i = 0; i < numberOfRows; i++) {
+			line = sc.nextLine();
+			str = new StringTokenizer(line);
+			for (int j = 0; j < data[0].length; j++) {
+				if (str.hasMoreTokens())
+					data[i][j] = str.nextToken(",");
 
+			}
 
-       // while(sc.hasNext()){
-   System.out.println("number of coulmns "+data[0].length);
+		}
 
-        for(i=379;i>=0;i--){
-          line=sc.nextLine();
-          str = new StringTokenizer(line);
-          for(int j=0;j<data[0].length;j++){
-             if(str.hasMoreTokens())//if((token=str.nextToken(",")).equals(null)!=true)
-                data[i][j]=str.nextToken(",");
-            
-               
-          }
-       
-          
-        }//for
-          
+		for (i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[0].length; j++)
+				System.out.print(data[i][j] + " ");
+			System.out.println();
+		}
+		
+	}// main
 
-       // }
-  for(i=0;i<data.length;i++){
-      for(int j=0;j<data[0].length;j++)
-          System.out.print(data[i][j]+" ");
-  System.out.println();
-  }
+	public static int numberOfRows(String filename) throws Exception {
 
+		Scanner sc = new Scanner(new File(filename));
+		sc.nextLine();
+		int count = 0;
 
-        
+		while (sc.hasNext()) {
+			sc.nextLine();
+			count++;
+		}
+		return count;
+	}// numberOfRows
 
-
-
-
-
-/*
-
-
-
-// TODO code application logic here
-    }
-*/
-    }//main
-
-    public static int numberOfRows(String filename)throws Exception{
-
-        Scanner sc = new Scanner(new File(filename));
-        sc.nextLine();
-        int count =0;
-
-        while(sc.hasNext()){
-            sc.nextLine();
-            count++;
-        }
-        return count;
-    }//numberOfRows
-
-}//class
+}// class
