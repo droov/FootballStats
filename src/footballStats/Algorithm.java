55832c09e116fd4ab523f63de7c2edf9d6f5d31d
KanishkKanoria <kanishk.kanoria@gmail.com>
2013-10-23 23:23:01 +0800

    Added 2nd Algorithm

--- src/footballStats/Algorithm.java
+++ src/footballStats/Algorithm.java
@@ -85,4 +85,60 @@ public class Algorithm {
 			diff = Math.abs(draw-away);
 		return diff;
 	}
+	
+	public void bettingTeamWise()throws Exception {
+		ParseCSV pcsv = new ParseCSV();
+		pcsv.importData();
+		ArrayList<String> headers = pcsv.getHeaders();
+		String[][] data = pcsv.getData();
+		char result = ' ';
+		int i,j,k,games,count=0;
+		double home, draw, away, payout, difference, ratio;
+				
+		String[][] columnMap = { { "FTR", "B365H", "B365D", "B365A", "BWH", "BWD", "BWA", "GBH", "GBD", "GBA", "IWH", "IWD", "IWA", "LBH", "LBD", "LBA", "PSH", "PSD", "PSA", "WHH", "WHD", "WHA", "SJH", "SJD", "SJA", "VCH", "VCD", "VCA", "BSH", "BSD", "BSA" }, { "6", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51" } };
+		String[] teamList = getTeams();
+		
+		String[][]resultList = new String[((columnMap[0].length-1)/3)*teamList.length][3];
+		System.out.println(resultList.length);
+		System.out.println(resultList[0].length);
+		
+		
+		for(k = 0; k < teamList.length; k++){			
+			for (j = 1; j < columnMap[0].length; j+=3) {
+				home = draw = ratio = away = difference = games = 0;
+				for (i = 0; i < data.length; i++) {
+					if ( data[i][2].equalsIgnoreCase(teamList[k]) || data[i][3].equalsIgnoreCase(teamList[k])) { // checking if the team to be evaluated is either the home or the away team
+						result = data[i][Integer.parseInt(columnMap[1][0])].charAt(0);	
+						home = Double.parseDouble(data[i][Integer.parseInt(columnMap[1][j])]);
+						draw = Double.parseDouble(data[i][Integer.parseInt(columnMap[1][j+1])]);
+						away = Double.parseDouble(data[i][Integer.parseInt(columnMap[1][j+2])]);
+						
+						if(result == 'H')
+							payout = home;
+						else if(result == 'D')
+							payout = draw;
+						else
+							payout = away;
+						difference = calculateDifference(home, draw, away);
+						
+						ratio += difference / payout;
+						games++;
+					}
+					else 
+						continue;
+				}
+				
+				resultList[count][0] = teamList[k];
+				resultList[count][1] = columnMap[0][j].substring(0, columnMap[0][j].length() - 1);
+				resultList[count][2] = Double.toString(ratio/games);
+				count++;
+				//bookerRatio.put(columnMap[0][j].substring(0, columnMap[0][j].length() - 1), ratio/games);
+			}
+		}
+		for(int m=0;m<resultList.length;m++){
+			for(int n=0;n<resultList[0].length;n++)
+				System.out.print(resultList[m][n] + "\t");
+			System.out.println();
+		}
+	}
 }
